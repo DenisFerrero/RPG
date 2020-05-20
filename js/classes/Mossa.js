73f4bf8aa030358dca551e2b3bb24466ca11c005
ANDY2099x <684andry@gmail.com>
2020-05-20 15:16:16 +0200

    nrihfvebiifev

---
+++ js/classes/Mossa.js
@@ -0,0 +1,35 @@
+//
+
+
+class Mossa {
+
+    valMax;
+    valMin;
+    pSuccess;
+    manaCost;
+    img;
+    soud;
+    type;
+    idMex;
+
+    constructor(valMax, valMin, pSuccess, manaCost, img, soud, type, idMex) {
+        this.valMax = valMax;
+        this.valMin = valMin;
+        this.pSuccess = pSuccess;
+        this.manaCost = manaCost;
+        this.img = img;
+        this.soud = sound;
+        this.type = type;
+        this.idMex = idMex;
+    }
+
+    dmg() {
+
+
+    }
+
+    precision() {
+
+    }
+
+}
