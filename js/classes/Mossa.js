b384674e6758d6bb93da1adae3a5388eef5feeb5
DenisFerrero <ferrerodenis2002@gmail.com>
2020-05-20 16:01:35 +0200

    new costructor reading from json + init json files

--- js/classes/Mossa.js
+++ js/classes/Mossa.js
@@ -8,18 +8,30 @@ class Mossa {
     pSuccess;
     manaCost;
     img;
-    soud;
+    sound;
     type;
     idMex;
 
-    constructor(valMax, valMin, pSuccess, manaCost, img, soud, type, idMex) {
+    constructor(valMax, valMin, pSuccess, manaCost, img, sound, type, idMex) {
         this.valMax = valMax;
         this.valMin = valMin;
         this.pSuccess = pSuccess;
         this.manaCost = manaCost;
         this.img = img;
-        this.soud = sound;
+        this.sound = sound;
         this.type = type;
+        this.idMex = idMex;
+    }
+
+    constructor(readedIndex, idMex) {
+        var newMossa = JSON.parse(MosseJSON)[readedIndex];
+        this.valMax = newMossa.valMax;
+        this.valMin = newMossa.valMin;
+        this.pSuccess = newMossa.pSuccess;
+        this.manaCost = newMossa.manaCost;
+        this.sound = newMossa.sound;
+        this.img = newMossa.img;
+        this.type = newMossa.type;
         this.idMex = idMex;
     }
 
