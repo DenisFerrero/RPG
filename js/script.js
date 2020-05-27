a0b0cc2a5fa83c166e525c270faf65edb9e88643
Tanja Prini <tanjaprini@gmail.com>
2020-05-27 08:34:33 +0200

    Dead players can't attack when selected

--- js/script.js
+++ js/script.js
@@ -87,16 +87,18 @@ function gameScript() {
     }
 
     self.attack = function() {
-        var action = document.getElementById("mosse").selectedIndex;
-        self.game.gamers[self.currentPlayer].attacks(action - 1);
-        self.game.turns++;
-        self.updateGUI();
-        if (self.game.turns == 2) {
-            setTimeout(() => {
-                self.game.evil.attacks();
-                self.game.turns = 0;
-                self.updateGUI();
-            }, 4500);
+        if (self.game.gamers[self.currentPlayer].life > 0) {
+            var action = document.getElementById("mosse").selectedIndex;
+            self.game.gamers[self.currentPlayer].attacks(action - 1);
+            self.game.turns++;
+            self.updateGUI();
+            if (self.game.turns == 2) {
+                setTimeout(() => {
+                    self.game.evil.attacks();
+                    self.game.turns = 0;
+                    self.updateGUI();
+                }, 4500);
+            }
         }
     }
 
